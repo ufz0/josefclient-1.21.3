6045d3757158780e4443683cec17269f8a0ba4c4
ufz0 <l.leutgeb@students.htl-leonding.ac.at>
2025-02-16 20:47:26 +0100

    Update JosefclientClient.java

--- src/client/java/at/korny/JosefclientClient.java
+++ src/client/java/at/korny/JosefclientClient.java
@@ -2,6 +2,8 @@ package at.korny;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.MinecraftVersion;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import static net.minecraft.server.command.CommandManager.*;
 
@@ -9,11 +11,12 @@ public class JosefclientClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("foo")
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("auernig")
 				.executes(context -> {
 					// For versions below 1.19, replace "Text.literal" with "new LiteralText".
 					// For versions below 1.20, remode "() ->" directly.
-					context.getSource().sendFeedback(() -> Text.literal("Called /foo"), false);
+					String instance = MinecraftVersion.create().getName();
+					context.getSource().sendFeedback(() -> Text.literal(instance), false);
 
 					return 1;
 				})));
