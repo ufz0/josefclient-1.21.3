f37a406bf7017f385e4426e2f017334b97b57c88
ufz0 <l.leutgeb@students.htl-leonding.ac.at>
2025-02-16 03:32:04 +0200

    Initial commit

---
+++ src/client/kotlin/at/cloudmesh/CloudClientClient.kt
@@ -0,0 +1,9 @@
+package at.cloudmesh
+
+import net.fabricmc.api.ClientModInitializer
+
+object CloudClientClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/at/cloudmesh/CloudClientDataGenerator.kt
@@ -0,0 +1,9 @@
+package at.cloudmesh
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object CloudClientDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
