d15e30ece1c3b83b4cbd334ffca7eeeab3f6f421
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-02-16 20:22:11 +0100

    Added / foo command for testing

--- src/client/java/at/korny/JosefclientClient.java
+++ src/client/java/at/korny/JosefclientClient.java
@@ -1,15 +1,21 @@
 package at.korny;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.text.Text;
+import static net.minecraft.server.command.CommandManager.*;
 
 public class JosefclientClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		for(int i = 0; i < 100; i++)
-		{
-			System.out.println("Josefclient " + i); //josef
-		}
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("foo")
+				.executes(context -> {
+					// For versions below 1.19, replace "Text.literal" with "new LiteralText".
+					// For versions below 1.20, remode "() ->" directly.
+					context.getSource().sendFeedback(() -> Text.literal("Called /foo"), false);
 
+					return 1;
+				})));
 	}
 }
--- src/main/java/at/korny/Josefclient.java
+++ src/main/java/at/korny/Josefclient.java
@@ -19,6 +19,6 @@ public class Josefclient implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Initializing Josef client");
 	}
 }
