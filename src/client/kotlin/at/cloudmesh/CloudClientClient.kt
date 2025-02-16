af2c405a4509847d308179dbc475a17c21b02a4b
ufz0 <l.leutgeb@students.htl-leonding.ac.at>
2025-02-16 15:51:00 +0200

    Added init message

--- src/client/kotlin/at/cloudmesh/CloudClientClient.kt
+++ src/client/kotlin/at/cloudmesh/CloudClientClient.kt
@@ -5,5 +5,6 @@ import net.fabricmc.api.ClientModInitializer
 object CloudClientClient : ClientModInitializer {
 	override fun onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		println("Initializing Cloud Client")
 	}
 }
