c97d76637967d17b2ef6066001a59a5bb7effe2d
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-02-17 19:36:46 +0100

    Removed unused code

--- src/client/java/at/korny/JosefclientClient.java
+++ src/client/java/at/korny/JosefclientClient.java
@@ -107,7 +107,6 @@ public class JosefclientClient implements ClientModInitializer {
 			context.drawText(client.textRenderer, "[Z] " + String.valueOf(z), 10, 45, 0xFFFFFF, true);
 			context.drawText(client.textRenderer, "[Biome] " + String.valueOf(biome), 10, 55, 0xFFFFFF, true);
 			context.drawText(client.textRenderer, "[Direction] " + direction, 10, 65, 0xFFFFFF, true);
-			context.drawText(client.textRenderer, "[Version]" + Version.version(), 10 , 75, 0xFFFFFF, true);
 		}
 	}
 	private void debugRenderer(DrawContext context, RenderTickCounter renderTickCounter) {
