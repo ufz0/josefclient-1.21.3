f5d4b57a4cde215cced78d281f57554af7562eb9
ufz0 <l.leutgeb@students.htl-leonding.ac.at>
2025-02-17 22:42:04 +0100

    Update MemoryUsageHelper.java
    Renamed class

--- src/client/java/at/korny/MemoryUsageHelper.java
+++ src/client/java/at/korny/MemoryUsageHelper.java
@@ -4,7 +4,7 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
 
-public class Memory {
+public class MemoryUsageHelper {
     public static int getMemoryUsagePercent() {
         MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
         MemoryUsage heapMemoryUsage = memoryBean.getHeapMemoryUsage();
@@ -14,9 +14,5 @@ public class Memory {
 
         int percentage = (int) Math.floor(((double) usedMemory / maxMemory) * 100.0);
         return percentage;
-    }
-
-    public static void main(String[] args) {
-        System.out.printf("Speichernutzung: %.2f%%%n", getMemoryUsagePercent());
     }
 }
