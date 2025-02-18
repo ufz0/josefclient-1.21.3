425791b4f3050bc3f82f70e02b3bee4b5f763ff2
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-02-18 17:15:18 +0100

    Cleaned up codebase

--- src/client/java/at/korny/JosefclientClient.java
+++ src/client/java/at/korny/JosefclientClient.java
@@ -2,22 +2,17 @@ package at.korny;
 
 import at.korny.utils.*;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.minecraft.entity.player.PlayerEntity;
 import java.util.function.BiConsumer;
 import static at.korny.utils.MemoryUsageHelper.getMemoryUsagePercent;
-import at.korny.utils.*;
 
 public class JosefclientClient implements ClientModInitializer {
 	private boolean rotating = false;
